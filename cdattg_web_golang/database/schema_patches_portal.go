4f15120e672fbe444577aaf6efa6614bb9560964
CRANDEYS <elfoniscristian@gmail.com>
2026-08-27 01:11:33 -0500

    feat(portal): migrar tablas del portal

---
+++ cdattg_web_golang/database/schema_patches_portal.go
@@ -0,0 +1,34 @@
+/**
+ * database: AutoMigrate de tablas del portal y semilleros.
+ * @author CRANDEYS
+ * @created 2026-08-26
+ */
+package database
+
+import (
+	"log"
+
+	"github.com/sena/cdattg-web-golang/models"
+)
+
+// patchAutoMigratePortalModels crea o verifica tablas del portal público.
+func patchAutoMigratePortalModels() error {
+	if err := DB.AutoMigrate(
+		&models.PortalBanner{},
+		&models.PortalPresentacion{},
+		&models.Semillero{},
+		&models.SemilleroLinea{},
+		&models.SemilleroIntegrante{},
+		&models.SemilleroProyecto{},
+		&models.BiogjgasBanner{},
+		&models.BiogjgasRevista{},
+		&models.BiogjgasBoletin{},
+		&models.BiogjgasPodcast{},
+		&models.BiogjgasConvocatoria{},
+		&models.BiogjgasActividad{},
+	); err != nil {
+		return err
+	}
+	log.Println("Esquema: tablas de portal y semillero verificadas")
+	return nil
+}

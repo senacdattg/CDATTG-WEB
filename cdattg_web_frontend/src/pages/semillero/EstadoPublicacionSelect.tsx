7712cafb24ba85f526d986d16aeeee40482846bf
CRANDEYS <elfoniscristian@gmail.com>
2026-08-27 09:03:08 -0500

    feat(semillero): selector de estado

---
+++ cdattg_web_frontend/src/pages/semillero/EstadoPublicacionSelect.tsx
@@ -0,0 +1,22 @@
+/**
+ * @module pages/semillero/EstadoPublicacionSelect
+ * @description Selector borrador / publicado / archivado.
+ * @author CRANDEYS
+ * @created 2026-08-26
+ */
+import type { PortalEstado } from '../../types/portal';
+
+type Props = Readonly<{ value: PortalEstado; onChange: (v: PortalEstado) => void }>;
+
+/**
+ * Estados de publicación del área BIOGIGAS.
+ */
+export function EstadoPublicacionSelect({ value, onChange }: Props) {
+  return (
+    <select className="input-field" value={value} onChange={(e) => onChange(e.target.value as PortalEstado)}>
+      <option value="borrador">Borrador</option>
+      <option value="publicado">Publicado</option>
+      <option value="archivado">Archivado</option>
+    </select>
+  );
+}
